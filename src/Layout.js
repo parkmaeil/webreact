b126428e40e9707fc36c2b6ab4a824f5de2e72ad
parkmaeil <bitcocom@empas.com>
2025-04-24 14:01:32 +0900

    react.js jwt

--- src/Layout.js
+++ src/Layout.js
@@ -24,6 +24,14 @@ export default function Layout({ children }) {
           {user ? (
             <>
               <Typography sx={{ mr: 2 }}>{user.email}님</Typography>
+
+              {/* user.roles 배열에 ROLE_ADMIN 이 포함되어 있으면 버튼을 렌더링 */}
+              {user.roles.includes('ROLE_ADMIN') && (
+                <Button color="inherit" component={Link} to="/admin">
+                  관리자 페이지
+                </Button>
+              )}
+
               {/* 로그아웃은 /logout 으로만 이동 */}
               <Button color="inherit" component={Link} to="/logout">
                 로그아웃
